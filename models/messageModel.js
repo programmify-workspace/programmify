e2984a8c8b2bf24aee5c78aa9db071b66a06e77e
Rachael <rachaelfavour2005@gmail.com>
2023-09-17 14:52:37 +0100

    New changes

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
-// require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "waitlist",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
@@ -16,23 +17,40 @@ db.connect((err) => {
   console.log('Connected to database as id ' + db.threadId);
 });
 
-
-function saveMessage(messageData, callback) {
+function saveMessage(messageData) {
   const { full_name, email } = messageData;
 
-  const sql = 'INSERT INTO messages (full_name, email) VALUES (?, ?)';
-  const values = [full_name, email];
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO messages (full_name, email) VALUES (?, ?)';
+    const values = [full_name, email];
 
-  db.query(sql, values, (error, results) => {
-    if (error) {
-      console.error('Error saving message to MySQL:', error);
-      callback(error, null);
-    } else {
-      console.log('Message saved to MySQL');
-      callback(null, results);
-    }
+    db.query(sql, values, (error, results) => {
+      if (error) {
+        console.error('Error saving message to MySQL:', error);
+        reject(error);
+      } else {
+        console.log('Message saved to MySQL');
+        resolve(results);
+      }
+    });
   });
 }
 
-module.exports = { saveMessage };
+function getWaitlistCount() {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT COUNT(*) AS waitlistCount FROM messages';
 
+    db.query(sql, (error, results) => {
+      if (error) {
+        console.error('Error fetching waitlist count from MySQL:', error);
+        reject(error);
+      } else {
+        const waitlistCount = results[0].waitlistCount;
+        console.log('Waitlist count retrieved from MySQL:', waitlistCount);
+        resolve(waitlistCount);
+      }
+    });
+  });
+}
+
+module.exports = { saveMessage, getWaitlistCount };
